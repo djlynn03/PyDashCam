320a55a3aa73cfe577e60378a5248c5e04ab465f
djlynn03 <djlynn03@gmail.com>
2022-05-13 01:19:00 -0500

    added more in-depth video history

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-from pickle import FRAME
 import cv2
 import datetime
 import time
 from dtypes import *
+import os
+from utils import *
+from gpiozero import Button
 
 VIDEO_LENGTH = 1 * 60 * 60 # 1 hour (3600 seconds)
 FRAMERATE = 30 # Frames per second
 # Video size should be 300-400 MB for 60 minutes at 30 fps for 640x480
 # May need adjustment for different video size and framerate
 
+# KEEP_TIME = 14 * 24 * 60 * 60 # 14 days (in seconds)
+KEEP_TIME = 60 # 60 seconds
+MAX_FOOTAGE_SIZE = 100 # in MB
+
+MODE = ['DEBUG', 'PROD'][0]
+RASPBERRY_PI_CONNECTED = True
+
+# Detect when the vehicle is running
+if RASPBERRY_PI_CONNECTED:
+    vehicle_on = Button(0)
+    vehicle_on.when_activated = lambda: print('Vehicle is running')
+    
+
 vid = cv2.VideoCapture(0)
 
 size = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-result = cv2.VideoWriter('result.mp4', cv2.VideoWriter_fourcc(*'MP4V'), FRAMERATE, size) # Result video
+result = cv2.VideoWriter('test.mp4', cv2.VideoWriter_fourcc(*'MP4V'), FRAMERATE, size) # Result video
 
 def save(frame_queue):
     result.write(frame_queue.dequeue())
@@ -25,17 +40,20 @@ while vid.isOpened():
     
     cv2.putText(frame, datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"), (5, int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
     
-    cv2.imshow('frame', frame)
+    if MODE == 'DEBUG':
+        cv2.imshow('frame', frame)
 
     buffer.enqueue(frame)
     buffer.foreach(save(buffer))
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
+    clean_files(KEEP_TIME, MAX_FOOTAGE_SIZE)
 
 # while not buffer.is_empty():
 #     result.write(buffer.dequeue())
     
-# vid.release()
-# result.release()
-# cv2.destroyAllWindows()
+vid.release()
+result.release()
+cv2.destroyAllWindows()
---
+++ utils.py
@@ -0,0 +1,28 @@
+import os
+import time
+
+def get_dir_size(path='.'):
+    total = 0
+    with os.scandir(path) as it:
+        for entry in it:
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_dir_size(entry.path)
+    return total
+
+def clean_files(keep_time, max_size):
+    '''
+    files will be kept if they were created in the last keep_time seconds
+    '''
+    for f in os.listdir('video/'):
+        if (os.path.isfile(os.path.join('video/', f)) and f.endswith('.mp4') and os.path.getmtime(os.path.join('video/', f)) < time.time() - keep_time):
+            os.remove(os.path.join('video/', f))
+            
+    while get_dir_size('video/') > (max_size * 1024 * 1024):
+        # Delete oldest files until the size is less than max_size
+        
+        # Get the oldest file
+        oldest = min(os.listdir('video/'), key=os.path.getmtime)
+        
+        os.remove(os.path.join('video/', oldest))
