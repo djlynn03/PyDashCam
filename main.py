b3fc08d826221def6bbd16ac0750b5bff1f7527d
djlynn03 <djlynn03@gmail.com>
2022-05-17 16:29:59 -0500

    video idle state

--- main.py
+++ main.py
@@ -16,44 +16,62 @@ KEEP_TIME = 60 # 60 seconds
 MAX_FOOTAGE_SIZE = 100 # in MB
 
 MODE = ['DEBUG', 'PROD'][0]
-RASPBERRY_PI_CONNECTED = True
+RASPBERRY_PI_CONNECTED = MODE == 'PROD'
 
+
+def start_video(trigger=None):
+    
+    vid = cv2.VideoCapture(0)
+
+    size = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+    name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    result = cv2.VideoWriter('video/' + name + '.mp4', cv2.VideoWriter_fourcc(*'MP4V'), FRAMERATE, size) # Result video
+
+    def save(frame_queue):
+        result.write(frame_queue.dequeue())
+        return result
+
+    buffer = FrameQueue(FRAMERATE * VIDEO_LENGTH) # Buffer queue to contain the last n frames, where n = framerate * video length
+    if trigger:
+        trigger.when_deactivated = vid.release()
+    
+    while vid.isOpened():
+        ret, frame = vid.read()
+
+        cv2.putText(frame, datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"), (5, int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+
+        if MODE == 'DEBUG':
+            cv2.imshow('frame', frame)
+
+        buffer.enqueue(frame)
+        buffer.foreach(save(buffer))
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+        
+        
+        clean_files(KEEP_TIME, MAX_FOOTAGE_SIZE)
+
+    # while not buffer.is_empty():
+    #     result.write(buffer.dequeue())
+
+    vid.release()
+    result.release()
+    
 # Detect when the vehicle is running
 if RASPBERRY_PI_CONNECTED:
-    vehicle_on = Button(0)
-    vehicle_on.when_activated = lambda: print('Vehicle is running')
+    vehicle_on = Button(2)
+    vehicle_on.when_activated = start_video(vehicle_on)
+else:
+    while True:
+        print("waiting")
+        cv2.waitKey(0)
+        print("started")
+        start_video()
+        
+    # vehicle_on.when_activated = set_state("RUNNING")
     
-
-vid = cv2.VideoCapture(0)
-
-size = (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-result = cv2.VideoWriter('test.mp4', cv2.VideoWriter_fourcc(*'MP4V'), FRAMERATE, size) # Result video
-
-def save(frame_queue):
-    result.write(frame_queue.dequeue())
-    return result
-
-buffer = FrameQueue(FRAMERATE * VIDEO_LENGTH) # Buffer queue to contain the last n frames, where n = framerate * video length
-
-while vid.isOpened():
-    ret, frame = vid.read()
+    # vehicle_on.when_deactivated = set_state("IDLE")
     
-    cv2.putText(frame, datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"), (5, int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-    
-    if MODE == 'DEBUG':
-        cv2.imshow('frame', frame)
-
-    buffer.enqueue(frame)
-    buffer.foreach(save(buffer))
-    
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
-    clean_files(KEEP_TIME, MAX_FOOTAGE_SIZE)
-
-# while not buffer.is_empty():
-#     result.write(buffer.dequeue())
-    
-vid.release()
-result.release()
 cv2.destroyAllWindows()
