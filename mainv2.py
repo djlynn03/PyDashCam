11890bfbe73c1c3d1ba0e9a9a79b38e234548932
djlynn03 <djlynn03@gmail.com>
2022-05-21 16:01:19 -0500

    added function to ensure raspberry pi is the device; updated gitignore; added more config settings

--- mainv2.py
+++ mainv2.py
@@ -1,54 +1,75 @@
 import cv2
 import datetime
-import time
 from dtypes import *
-import os
 from utils import *
 from gpiozero import Button
-from signal import pause
+import io
 
-VIDEO_LENGTH = 1 * 60 * 60 # 1 hour (3600 seconds)
-FRAMERATE = 30 # Frames per second
-# Video size should be 300-400 MB for 60 minutes at 30 fps for 640x480
-# May need adjustment for different video size and framerate
+try:
+    from signal import pause
+except ImportError:
+    print("signal is not installed or not supported on this platform")
+    exit()
 
-# KEEP_TIME = 14 * 24 * 60 * 60 # 14 days (in seconds)
-KEEP_TIME = 600 # 60 seconds
-MAX_FOOTAGE_SIZE = 100 # in MB
+FRAMERATE = 15 # Frames per second
+# This value is tricky because it is not always the same as the framerate of the camera
+# The camera I am using is supposed to be 30 fps, but 15 fps is the number that records correctly
 
-MODE = ['DEBUG', 'PROD'][1]
-RASPBERRY_PI_CONNECTED = MODE == 'PROD'
+VIDEO_LENGTH = None # in seconds
+# Can be used to specify the maximum length of a single video. If a video is recorded longer than this, only the last X seconds will be saved.
 
+MAX_KEEP_TIME = 14 * 24 * 60 * 60 # 14 days (in seconds)
+MAX_FOOTAGE_SIZE = 1000 # in MB
+# Maximum footage length and size can be specified. Any footage older that X seconds or larger than Y MB will be deleted.
+# Can be set to None to disable this feature.
 
+def is_raspberrypi():
+    try:
+        with io.open('/sys/firmware/devicetree/base/model', 'r') as f:
+            if 'raspberrypi' in f.read().lower():
+                return True
+    except Exception:
+        pass
+    return False
+
+RASPBERRY_PI_CONNECTED = is_raspberrypi()
+
+if not RASPBERRY_PI_CONNECTED:
+    print("This script is only compatible with Raspberry Pi")
+    exit()
+    
 class Capture:
     def __init__(self, trigger: Button):
         self.running = True
-        self.vid = cv2.VideoCapture(-1)
-        self.size = (int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+        
+        self.vid = cv2.VideoCapture(-1) # -1 automatically selects the camera
+        
+        
         self.name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        self.result = cv2.VideoWriter('video/' + self.name + '.mp4', cv2.VideoWriter_fourcc(*'MP4V'), FRAMERATE, self.size) # Result video
-        self.buffer = FrameQueue(FRAMERATE * VIDEO_LENGTH) # Buffer queue to contain the last n frames, where n = framerate * video length
+        self.size = (int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))) # default size is 640x480
+
+        self.result = cv2.VideoWriter('video/' + self.name + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), FRAMERATE, self.size) # Result video
+        
+        self.buffer = None
+        if VIDEO_LENGTH:
+            self.buffer = FrameQueue(FRAMERATE * VIDEO_LENGTH) # Buffer queue to contain the last n frames, where n = framerate * video length
+            
         self.trigger = trigger
         self.trigger.when_released = self.stop
         
-        while self.vid.isOpened() and self.running:
+        while self.vid.isOpened() and self.running and self.trigger.is_pressed:
             self.ret, self.frame = self.vid.read()
 
             cv2.putText(self.frame, datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"), (5, int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
 
-            if MODE == 'DEBUG':
-                cv2.imshow('frame', self.frame)
+            if self.buffer:
+                self.buffer.enqueue(self.frame)
+                self.buffer.foreach(self.save())
+            else:
+                self.result.write(self.frame)
 
-            self.buffer.enqueue(self.frame)
-            self.buffer.foreach(self.save())
-            if not RASPBERRY_PI_CONNECTED:
-                if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
             
-        # clean_files(KEEP_TIME, MAX_FOOTAGE_SIZE)
-
-        # while not buffer.is_empty():
-        #     result.write(buffer.dequeue())
+        clean_files(MAX_KEEP_TIME, MAX_FOOTAGE_SIZE)
 
         self.vid.release()
         self.result.release()
@@ -72,16 +93,16 @@ def start_video():
     global cap
     cap = Capture(vehicle_on)
 
-def stop_video():
-    print("stopping")
-    global cap
-    if cap:
-        cap.stop()
+# def stop_video():
+#     print("stopping")
+#     global cap
+#     if cap:
+#         cap.stop()
     
 # Detect when the vehicle is running
 if RASPBERRY_PI_CONNECTED:
     vehicle_on.when_pressed = start_video
-    vehicle_on.when_released = stop_video
+    # vehicle_on.when_released = stop_video
     pause()
 else:
     while True:
@@ -92,5 +113,5 @@ else:
     # vehicle_on.when_activated = set_state("RUNNING")
     
     # vehicle_on.when_deactivated = set_state("IDLE")
-if RASPBERRY_PI_CONNECTED:
-    cv2.destroyAllWindows()
+# if RASPBERRY_PI_CONNECTED:
+#     cv2.destroyAllWindows()
