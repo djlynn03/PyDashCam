02600370ce4b9a4e1d573faeb67ccfa4c1a596a3
djlynn03 <djlynn03@gmail.com>
2022-05-23 14:51:25 -0500

    fixed video length limit

--- main.py
+++ main.py
@@ -38,20 +38,26 @@ class Capture:
         self.trigger = trigger
         self.trigger.when_released = self.stop
         
+        self.num_frames = 0
         while self.vid.isOpened() and self.running and self.trigger.is_pressed:
             self.ret, self.frame = self.vid.read()
 
             cv2.putText(self.frame, datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"), (5, int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
             
-            if int(self.vid.get(cv2.CAP_PROP_FRAME_COUNT)) >= VIDEO_LENGTH * FRAMERATE: # Start new video if the current one has reached the maximum length
+            if self.num_frames >= VIDEO_LENGTH * FRAMERATE: # Start new video if the current one has reached the maximum length
+                print("Starting new video...")
                 self.vid.release()
                 self.result.release()
                 self.create_video()
+                self.num_frames = 0
+                print("New video created")
                 continue
                 
             self.result.write(self.frame)
 
             clean_files(MAX_KEEP_TIME, MAX_FOOTAGE_SIZE)
+            
+            self.num_frames += 1
 
         self.vid.release()
         self.result.release()
@@ -73,8 +79,7 @@ class Capture:
         self.vid = cv2.VideoCapture(-1) # -1 automatically selects the camera
         name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         size = (VIDEO_WIDTH, VIDEO_HEIGHT) # default size is 640x480
-        self.result = cv2.VideoWriter('video/' + name + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), FRAMERATE, size) # Result video
-        
+        self.result = cv2.VideoWriter('video/' + name + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), FRAMERATE, size) # Result video        
 
 vehicle_on = Button(2)
 global cap 
