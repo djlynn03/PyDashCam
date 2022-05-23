636a31e6e176929919cfd423b6d1f6caad31795d
djlynn03 <djlynn03@gmail.com>
2022-05-23 14:30:26 -0500

    created config file; changed event workflow

---
+++ config.py
@@ -0,0 +1,19 @@
+FRAMERATE = 15 # Frames per second
+# This value is tricky because it is not always the same as the framerate of the camera
+# The camera I am using is supposed to be 30 fps, but 15 fps is the number that records correctly
+
+VIDEO_WIDTH = 640
+VIDEO_HEIGHT = 480
+# Dimensions of the video
+
+MAX_KEEP_TIME = 14 * 24 * 60 * 60 # 14 days (in seconds)
+MAX_FOOTAGE_SIZE = 5000 # in MB
+
+# Maximum footage length and size can be specified. Any footage older that X seconds or larger than Y MB will be deleted.
+# Can be set to None to disable this feature.
+
+VIDEO_LENGTH = 30 # 30 seconds (in seconds)
+# Videos are recorded in chunks of X seconds so that there will be less storage overflow.
+
+REBOOT_ON_STOP = True
+# Reboot the machine when the vehicle turns off
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import datetime
 from dtypes import *
 from utils import *
+from config import *
 from gpiozero import Button
 import io
 
@@ -10,18 +11,6 @@ try:
 except ImportError:
     print("signal is not installed or not supported on this platform")
     exit()
-
-FRAMERATE = 15 # Frames per second
-# This value is tricky because it is not always the same as the framerate of the camera
-# The camera I am using is supposed to be 30 fps, but 15 fps is the number that records correctly
-
-VIDEO_LENGTH = None # in seconds
-# Can be used to specify the maximum length of a single video. If a video is recorded longer than this, only the last X seconds will be saved.
-
-MAX_KEEP_TIME = 14 * 24 * 60 * 60 # 14 days (in seconds)
-MAX_FOOTAGE_SIZE = 5000 # in MB
-# Maximum footage length and size can be specified. Any footage older that X seconds or larger than Y MB will be deleted.
-# Can be set to None to disable this feature.
 
 def is_raspberrypi():
     try:
@@ -42,18 +31,10 @@ class Capture:
     def __init__(self, trigger: Button):
         self.running = True
         
-        self.vid = cv2.VideoCapture(-1) # -1 automatically selects the camera
+        self.vid = None
+        self.result = None # Resulting video
+        self.create_video() # Populates the result and vid variables
         
-        
-        self.name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        self.size = (int(self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))) # default size is 640x480
-
-        self.result = cv2.VideoWriter('video/' + self.name + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), FRAMERATE, self.size) # Result video
-        
-        self.buffer = None
-        if VIDEO_LENGTH:
-            self.buffer = FrameQueue(FRAMERATE * VIDEO_LENGTH) # Buffer queue to contain the last n frames, where n = framerate * video length
-            
         self.trigger = trigger
         self.trigger.when_released = self.stop
         
@@ -61,27 +42,39 @@ class Capture:
             self.ret, self.frame = self.vid.read()
 
             cv2.putText(self.frame, datetime.datetime.now().strftime("%m/%d/%y %H:%M:%S"), (5, int(self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT))-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-
-            if self.buffer:
-                self.buffer.enqueue(self.frame)
-                self.buffer.foreach(self.save())
-            else:
-                self.result.write(self.frame)
+            
+            if int(self.vid.get(cv2.CAP_PROP_FRAME_COUNT)) >= VIDEO_LENGTH * FRAMERATE: # Start new video if the current one has reached the maximum length
+                self.vid.release()
+                self.result.release()
+                self.create_video()
+                continue
+                
+            self.result.write(self.frame)
 
             clean_files(MAX_KEEP_TIME, MAX_FOOTAGE_SIZE)
 
         self.vid.release()
         self.result.release()
 
-    def save(self):
-        self.result.write(self.buffer.dequeue())
-        return self.result
+    # def save(self):
+    #     self.result.write(self.buffer.dequeue())
+    #     return self.result
     
     def stop(self):
         self.vid.release()
         print("stopping")
         self.running = False
+        if REBOOT_ON_STOP:
+            print("rebooting...")
+            os.system("sudo shutdown -r now")
         return
+    
+    def create_video(self):
+        self.vid = cv2.VideoCapture(-1) # -1 automatically selects the camera
+        name = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        size = (VIDEO_WIDTH, VIDEO_HEIGHT) # default size is 640x480
+        self.result = cv2.VideoWriter('video/' + name + '.mp4', cv2.VideoWriter_fourcc(*'mp4v'), FRAMERATE, size) # Result video
+        
 
 vehicle_on = Button(2)
 global cap 
@@ -92,7 +85,6 @@ def start_video():
     global cap
     cap = Capture(vehicle_on)
 
-    
 # Detect when the vehicle is running
 vehicle_on.when_pressed = start_video
 
